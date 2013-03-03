32d4b47af928dfab915e828fbcff894ea69fbbc9
nowinski2009@gmail.com <nowinski2009@gmail.com@3c4577e9-5d55-1280-ef87-42a95ba53da5>
2013-03-03 05:33:21 +0000

    vod pl poprawione

--- src/plugin.video.mrknow/host/vodpl.py
+++ src/plugin.video.mrknow/host/vodpl.py
@@ -65,25 +65,21 @@ class vodpl:
 
         valTab = [] 
         strTab = []    
-        vod_filmyget = '[{"method":"cmsQuery","id":"020E5AD0-006E-41B7-B2F0-35B64BF6FA27","jsonrpc":"2.0","params":{"sort":"TITLE_ASC","method":"aggregates","args":{"withoutDRM":"True","device":"mobile","names":"genres","payment":["-svod","-ppv"],"channel":"'+category+'"},"context":"onet/vod"}}]'
+        vod_filmyget = '{"method":"cmsQuery","id":"020E5AD0-006E-41B7-B2F0-35B64BF6FA27","jsonrpc":"2.0","params":{"sort":"TITLE_ASC","method":"aggregates","args":{"withoutDRM":"True","device":"mobile","names":"genres","payment":["-svod","-ppv"],"channel":"'+category+'"},"context":"onet/vod"}}'
         
-        vod_filmy = self.getpage(vod_filmyget)
-        vod_filmy = eval(vod_filmy)
+        vod_filmy = eval(self.getpage(vod_filmyget))
         print ("VOD",vod_filmy)
         #vod_filmy = [{"jsonrpc": "2.0", "result": {"metrics": {"count": 10, "current": 1, "total": 0, "offset": 0}, "timestamp": 1362216678, "data": [{"structureType": "aggregate", "aggregateName": "genres", "aggregateObject": null, "structureRevision": "1", "metrics": null, "items": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "animowany", "value": 7}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "biograficzny", "value": 16}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "dla dzieci", "value": 16}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "dramat", "value": 160}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "historyczny", "value": 11}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "horror", "value": 24}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "komedia", "value": 125}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "kryminalny", "value": 19}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "melodramat", "value": 9}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "muzyczny", "value": 13}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "obyczajowy", "value": 105}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "psychologiczny", "value": 47}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "sci-fi", "value": 10}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "sensacyjny", "value": 35}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "thriller", "value": 17}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "wojenny", "value": 22}]}], "error": {"message": "", "code": 0}}, "id": "020E5AD0-006E-41B7-B2F0-35B64BF6FA27"}]
-
-        
-        
-        for o in vod_filmy:
-            
-            match = re.compile("'name': '(.*?)', 'value': (.*?)}", re.DOTALL).findall(str(o))
-            for i in range(len(match)):
-                strTab.append(match[i][0])
-                strTab.append(match[i][1])
-                strTab.append(o["id"])
-                valTab.append(strTab)
-                strTab = []
-                valTab.sort(key = lambda x: x[0])
+        print vod_filmy
+        print vod_filmy["result"]["data"]
+        print vod_filmy["id"]     
+        for e in vod_filmy["result"]["data"][0]["items"]:
+            strTab.append(e["name"])
+            strTab.append(e["value"])
+            strTab.append(vod_filmy["id"])
+            valTab.append(strTab)
+            strTab = []
+            valTab.sort(key = lambda x: x[0])
 #             
         print valTab
         for i in valTab:
@@ -115,24 +111,34 @@ class vodpl:
 #        test3 = {"method":"cmsGet","id":"B51DE533-CCEC-47CA-BA71-5F8DDFF02AEA","jsonrpc":"2.0","params":{"context":"onet/vod","id":"86419","object":"Video"}}
         
         vod_getitems = '[{"method":"cmsQuery","id":"'+id2+'","jsonrpc":"2.0","params":{"sort":"DATE_DESC","method":"search","args":{"withoutDRM":"True","device":"mobile","payment":["-svod","-ppv"],"genre":"'+title+'","channel":"'+category+'"},"context":"onet/vod","range":[0,10000]}}]'
-        vod_items = self.getpage(vod_getitems)
-        match1 = re.compile('"structureType": "video", "title": "(.*?)",', re.DOTALL).findall(str(vod_items))
-        match2 = re.compile('"videoId": "(.*?)",', re.DOTALL).findall(str(vod_items))
-        match3 = re.compile('"ckmId": "(.*?)",', re.DOTALL).findall(str(vod_items))  
-        match4 = re.compile('"poster": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "(.*?)",', re.DOTALL).findall(str(vod_items))  
-         
-        print match1, len(match1) 
-        print match4,len(match4)
+        vod_items = eval(self.getpage(vod_getitems))
+ #       match1 = re.compile('"structureType": "video", "title": "(.*?)",', re.DOTALL).findall(str(vod_items))
+ #       match2 = re.compile('"videoId": "(.*?)",', re.DOTALL).findall(str(vod_items))
+ #       match3 = re.compile('"ckmId": "(.*?)",', re.DOTALL).findall(str(vod_items))  
+ #       match4 = re.compile('"poster": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "(.*?)",', re.DOTALL).findall(str(vod_items))  
         
+        #vod_items = [{"jsonrpc": "2.0", "result": {"metrics": {"count": 10000, "current": 7, "total": 7, "offset": 0}, "timestamp": 1362280349, "data": [{"structureType": "video", "title": "Disco Robaczki", "mobile": 1, "structureRevision": "1.1", "countries": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "Niemcy", "value": ""}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "Dania", "value": ""}], "videoId": "80317", "poster": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "6d8894db29c3e87f6f2db04ad258e2c4", "imageMimeType": "", "appId": 1}, "channels": [{"structureType": "meta.category", "name": "Filmy", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "filmy", "categoryId": 1336}, {"structureType": "meta.category", "name": "DZIECKO", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "dziecko", "categoryId": 1399}, {"structureType": "meta.category", "name": "mlodziez", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "mlodziez", "categoryId": 1383}], "duration": 4510, "year": 2008, "date": 1349038860, "ckmId": "351763026", "leadMedia": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "5a494526d403f1ad5e3756cb12f572c1", "imageMimeType": "", "appId": 1}, "genres": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "animowany", "value": ""}]}, {"structureType": "video", "title": "Prawdziwa historia kota w butach", "mobile": 1, "structureRevision": "1.1", "countries": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "Francja", "value": ""}], "videoId": "89909", "poster": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "1056cedc525f1283abc7e1c56d78a682", "imageMimeType": "", "appId": 1}, "channels": [{"structureType": "meta.category", "name": "Filmy", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "filmy", "categoryId": 1336}, {"structureType": "meta.category", "name": "DZIECKO", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "dziecko", "categoryId": 1399}, {"structureType": "meta.category", "name": "mlodziez", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "mlodziez", "categoryId": 1383}], "duration": 4728, "year": 2009, "date": 1340958960, "ckmId": "181629.528241027", "leadMedia": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "7c4db576b5499b88af8658a564c43bde", "imageMimeType": "", "appId": 1}, "genres": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "animowany", "value": ""}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "dla dzieci", "value": ""}]}, {"structureType": "video", "title": "Gwiazda Kopernika", "mobile": 1, "structureRevision": "1.1", "videoId": "87935", "poster": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "5868ca784b9fd188fdb1ef60c2594ec1", "imageMimeType": "", "appId": 1}, "channels": [{"structureType": "meta.category", "name": "Filmy", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "filmy", "categoryId": 1336}, {"structureType": "meta.category", "name": "DZIECKO", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "dziecko", "categoryId": 1399}], "duration": 5388, "year": 2009, "date": 1340886600, "ckmId": "181633.156854594", "leadMedia": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "6751a31925a378bc6ca3639c23a66534", "imageMimeType": "", "appId": 1}, "genres": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "animowany", "value": ""}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "dla dzieci", "value": ""}]}, {"structureType": "video", "title": "Renifer Niko ratuje \u015bwi\u0119ta", "mobile": 1, "structureRevision": "1.1", "countries": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "Dania", "value": ""}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "Niemcy", "value": ""}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "Finlandia", "value": ""}], "videoId": "89913", "poster": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "6422dd8fa8033bb081f982a26cab629e", "imageMimeType": "", "appId": 1}, "channels": [{"structureType": "meta.category", "name": "Filmy", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "filmy", "categoryId": 1336}, {"structureType": "meta.category", "name": "DZIECKO", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "dziecko", "categoryId": 1399}], "duration": 4645, "year": 2008, "date": 1340184420, "ckmId": "180121.1486278589", "leadMedia": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "88555a8c6e7eefe856de9891e92ecbef", "imageMimeType": "", "appId": 1}, "genres": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "animowany", "value": ""}]}, {"structureType": "video", "title": "Pszcz\u00f3\u0142ka Julia", "mobile": 1, "structureRevision": "1.1", "countries": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "W\u0142ochy", "value": ""}], "videoId": "89911", "poster": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "e20360a9a7022b7233e4e8d7b45511bd", "imageMimeType": "", "appId": 1}, "channels": [{"structureType": "meta.category", "name": "Filmy", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "filmy", "categoryId": 1336}, {"structureType": "meta.category", "name": "DZIECKO", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "dziecko", "categoryId": 1399}], "duration": 4502, "year": 2003, "date": 1339505640, "ckmId": "179020.882699861", "leadMedia": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "1fdb84b18ba213f8ca26660957d41032", "imageMimeType": "", "appId": 1}, "genres": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "animowany", "value": ""}]}, {"structureType": "video", "title": "Gdzie jest Nowy Rok?", "mobile": 1, "structureRevision": "1.1", "countries": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "Polska", "value": ""}], "videoId": "89902", "channels": [{"structureType": "meta.category", "name": "Filmy", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "filmy", "categoryId": 1336}, {"structureType": "meta.category", "name": "DZIECKO", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "dziecko", "categoryId": 1399}], "date": 1327528860, "year": 2006, "duration": 1389, "ckmId": "154162.827287125", "leadMedia": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "d445bb7bbf79df3009babe0f930104e9", "imageMimeType": "", "appId": 1}, "genres": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "animowany", "value": ""}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "dla dzieci", "value": ""}]}, {"countries": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "USA", "value": ""}], "displayStyleArgs": {"adult": true}, "leadMedia": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "edd9e5b8653d222b37bf6456a65c7e29", "imageMimeType": "", "appId": 1}, "title": "Hellboy - Krew i \u017celazo", "mobile": 1, "structureRevision": "1.1", "year": 2007, "videoId": "82989", "channels": [{"structureType": "meta.category", "name": "Filmy", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "filmy", "categoryId": 1336}, {"structureType": "meta.category", "name": "facet", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "facet", "categoryId": 1381}, {"structureType": "meta.category", "name": "mlodziez", "structureRevision": "1.1.1", "modificationEpoch": 0, "path": "", "codeName": "mlodziez", "categoryId": 1383}], "duration": 4382, "poster": {"structureType": "content.media.image", "structureRevision": "2.1.1", "modificationEpoch": 0, "imageId": "4863009b8446a1a6629d2093629bea57", "imageMimeType": "", "appId": 1}, "date": 1325800860, "ckmId": "151607.701749722", "branding": {"brandingId": 18, "structureRevision": "1", "structureType": "videobranding", "bgColor": "#00000000", "title": "Ostra anima"}, "structureType": "video", "genres": [{"structureRevision": "1", "structureType": "simpleattribute", "name": "animowany", "value": ""}, {"structureRevision": "1", "structureType": "simpleattribute", "name": "horror", "value": ""}]}], "error": {"message": "", "code": 0}}, "id": "020E5AD0-006E-41B7-B2F0-35B64BF6FA27"}]
+        print vod_items[0]["result"] 
         print vod_items
-        
-        for i in range(len(match1)):
-            if len(match4)>0:
-                image = imgurl + match4[i] + ',10,1.jpg'
+        for e in vod_items[0]["result"]["data"]:
+            print e
+            print e["title"]
+            print e["videoId"] 
+            print e["ckmId"] 
+            if 'poster' in e:
+                image = imgurl + e['poster']["imageId"] + ',10,1.jpg'
             else:
-                image =''          
-            #add(    service,    name,            category, title, iconimage, url, desc, rating, folder = True, isPlayable = True,id1='',id2='')       
-            self.add('vodpl', 'playSelectedMovie', 'None', match1[i].decode('utf-8'), image, 'None', 'None', 'None', False, False,match2[i],match3[i])
+                image = ''
+            self.add('vodpl', 'playSelectedMovie', 'None', e["title"], image, 'None', 'None', 'None', False, False,e["videoId"],e["ckmId"] )
+            
+            
+  #      for i in range(len(match1)):
+  #          if len(match4)>0:
+  #              image = imgurl + match4[i] + ',10,1.jpg'
+  #          else:
+  #              image =''          
+  #          #add(    service,    name,            category, title, iconimage, url, desc, rating, folder = True, isPlayable = True,id1='',id2='')       
+  #          self.add('vodpl', 'playSelectedMovie', 'None', match1[i].decode('utf-8'), image, 'None', 'None', 'None', False, False,match2[i],match3[i])
         
         
         #req = urllib2.Request(url)
